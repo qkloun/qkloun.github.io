5afb16f893b7c398da99d2e9653beab1d5896925
Yunus Karatas <157628444+qkloun@users.noreply.github.com>
2025-09-16 15:30:22 -0400

    Update script.js
    Added mobile var bar menu js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-//Theme Button
+//Theme toggle
 const ThemeToggle = document.getElementById('theme-toggle');
 const ProfileImg = document.getElementById('profile-img');
 
-ThemeToggle.addEventListener('click', ()=> {
-    document.body.classList.toggle('light-mode');
-    const Icon = ThemeToggle.querySelector('i');
+console.log("ThemeToggle found:", ThemeToggle);
+console.log("ProfileImg found:", ProfileImg);
 
+ThemeToggle.addEventListener('click', () => {
+    const Icon = ThemeToggle.querySelector('i');
+    console.log("Icon found:", Icon);
+    
     if(document.body.classList.contains('light-mode')){
-        Icon.className = 'fas fa-moon';
-        ProfileImg.src ='images/wonderd.png';
-    } else {
+        document.body.classList.remove('light-mode');
         Icon.className = 'fas fa-sun';
-        ProfileImg.src = 'images/Wonder.png';
+        ProfileImg.src = 'images/wonder.png';
+    } else {
+        document.body.classList.add('light-mode');
+        Icon.className = 'fas fa-moon';
+        ProfileImg.src = 'images/wonderd.png';
     }
+});
+
+//Mobile Menu button
+document.getElementById('mobile-menu-btn').addEventListener('click', function() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
 });
